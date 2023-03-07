25590652b7f3eb9fbc398b194c77ab67164b4dfc
johnstonew <johnstonewahome@gmail.com>
2023-03-07 12:29:18 -0800

    added print comb3 file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for num in range(0, 10):
+    for num2 in range(num, 10):
+        if num == 8 and num2 == 9:
+            print("{0:d}{1:d}".format(num, num2))
+        elif num != num2:
+            print("{0:d}{1:d}, ".format(num, num2), end="")
