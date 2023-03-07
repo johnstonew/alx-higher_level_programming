eabb9198240f432fc56b8875cda7535a24447c8e
johnstonew <johnstonewahome@gmail.com>
2023-03-07 13:41:22 -0800

    changed method for getting last num

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    number = str(number)
-    result = number[-1]
+    if number < 0:
+        number *= -1
+    result = number % 10
     print("{}".format(result), end="")
     return result
