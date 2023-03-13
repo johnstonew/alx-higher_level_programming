ebe3376b5dc5f55c0e0bdc6f47b4ce2e30319feb
Johnstone Wahome <johnstonewahome@gmail.com>
2023-03-13 21:54:56 +0300

    increased default value of num

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return "None"
+        return None
     else:
-        max_num = 0
+        max_num = -200
         for i in range(len(my_list)):
             if my_list[i] > max_num:
                 max_num = my_list[i]
