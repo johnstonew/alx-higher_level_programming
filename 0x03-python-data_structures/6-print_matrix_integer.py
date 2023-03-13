29be9388aa58df7dd25ddd92f5746c1465cd497c
Johnstone Wahome <johnstonewahome@gmail.com>
2023-03-13 21:02:18 +0300

    6-print_matrix_integer exercise

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for arr in matrix:
+        for d in arr:
+            print("{:d}".format(d), end=" " if d != arr[-1] else " ")
+        print()
