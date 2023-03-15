f900c0c80ddc58beed23d48cbc645888fcad094b
Johnstone Wahome <johnstonewahome@gmail.com>
2023-03-15 22:17:46 +0300

    10-best_score.py task

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        my_num = 0
+        my_key = 'a'
+        for key in a_dictionary.keys():
+            if a_dictionary[key] > my_num:
+                my_num = a_dictionary[key]
+                my_key = key
+        return my_key
