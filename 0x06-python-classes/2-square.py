4042436b3c97ebc9aeb34fceda9bbf70d4586733
Johnstone Wahome <johnstonewahome@gmail.com>
2023-03-28 13:22:16 +0300

    Size validation test

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Define square class
+"""
+
+
+class Square:
+    """ Sqaure class """
+    def __init__(self, size=0):
+        """ New Sqaure instance
+        Args:
+        size (int): Size of the new square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
