6ddbad83f2a5744904150f24b51f470dc4c312ae
Johnstone Wahome <johnstonewahome@gmail.com>
2023-03-28 20:50:40 +0300

    update text

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -60,9 +60,10 @@ class Square:
             print()
         if self.__size == 0:
             print("")
+            return
 
         [print("") for i in range(0, self.__position[1])]
-        for i in range(self.__size):
+        for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
