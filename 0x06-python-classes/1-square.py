5ce451ed116ab5e8011d0b921d4825ee27ec46b4
Johnstone Wahome <johnstonewahome@gmail.com>
2023-03-28 13:15:22 +0300

    Square with size test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Define square class
+"""
+
+
+class Square:
+    """ Sqaure class """
+    def __init__(self, size):
+        """ New Sqaure instance
+        Args:
+        size (int): Size of the new square
+        """
+        self.__size = size
