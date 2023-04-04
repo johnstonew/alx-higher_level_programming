e978daf54d29ccdf316ccd11b86041fcf926e883
Johnstone Wahome <johnstonewahome@gmail.com>
2023-04-04 19:01:30 +0300

    added comments

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" method that prevents the user
+from dynamically creating new instance
+that prevents the user from dynamically
+creating new instance
+"""
 
 
 class LockedClass():
+    """ function to prevent other attributes
+    """
     __slots__ = ["first_name"]
