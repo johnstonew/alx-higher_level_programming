0f94e8c4f31c0869d81cf9150f775a5c9c0704e4
Johnstone Wahome <johnstonewahome@gmail.com>
2023-04-12 23:42:46 +0300

    test 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+returns the dictionary description
+with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """
+    function that returns the dictionary description
+    """
+    return obj.__dict__
