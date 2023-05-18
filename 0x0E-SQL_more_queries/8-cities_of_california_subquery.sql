74b11919de592de1c5519814eb862ca7a2333556
Johnstone Wahome <johnstonewahome@gmail.com>
2023-05-18 17:15:46 +0300

    8. Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- script that lists all the cities of
+-- California that can be found in the database hbtn_0d_usa
+SET @state_id := (SELECT id FROM states WHERE name = 'California');
+SELECT * FROM cities WHERE state_id = @state_id ORDER BY id ASC;
