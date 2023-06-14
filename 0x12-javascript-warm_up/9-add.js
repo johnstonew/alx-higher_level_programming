67afe02f17e602548b5eb30e26359aa9c332069a
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-14 19:56:08 +0300

    simplified code

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -9,9 +9,4 @@ const secondArgument = process.argv[3];
 const num1 = parseInt(firstArgument);
 const num2 = parseInt(secondArgument);
 
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('Invalid input. Please provide two integers.');
-} else {
-  const result = add(num1, num2);
-  console.log(result);
-}
+add(num1, num2);
