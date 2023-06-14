8f8b34cf09fc70b04a2d1cc0d38f91cfc80d1903
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-14 19:50:52 +0300

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const sizeArgument = process.argv[2];
+const size = parseInt(sizeArgument);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let j = 0; j < size; j++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
