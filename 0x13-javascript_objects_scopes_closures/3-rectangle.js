b6f6a08c3268986e1c394a7e6b27bd0dba483722
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-14 22:12:23 +0300

    3. Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width <= 0 || this.height <= 0) {
+      return;
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
