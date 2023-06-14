dcfbf7f109096f6ed2d0276a28d4cf70b1d686c5
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-14 22:49:46 +0300

    num

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-exports.converter = function(base) {
-  if (base <= 1) {
-    return;
-  }
-
-  exports.converter = function(number) {
-    if (number >= base) {
-      exports.converter(Math.floor(number / base));
-    }
-    process.stdout.write((number % base).toString());
+exports.converter = function (base) {
+  return function (number) {
+    return number.toString(base);
   };
 };
