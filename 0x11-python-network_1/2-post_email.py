17b61425e35f27ca2372a0c940b6fc3ba26f08ba
Johnstone Wahome <johnstonewahome@gmail.com>
2023-06-30 22:39:46 +0300

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Python script that takes in a URL and an email, sends a POST
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+
+print('Response body:')
+print(body)
