2df808de535909eccbd27ac70620ad50dbaaf35c
Johnstone Wahome <johnstonewahome@gmail.com>
2023-07-04 17:53:39 +0300

    url lib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """script that fetches a url"""
 import urllib.request
+from urllib.error import URLError
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
+def get_body_decode():
+    try:
+        req = Request("https://alx-intranet.hbtn.io/status")
+        with urlopen(req) as response:
+            body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except URLError as e:
+        if hasattr(e, "reason"):
+            print(e.reason)
+        elif hasattr(e, "code"):
+            print(e.code)
 
-print('- Body response:')
-print('\t', body)
+
+if __name__ == "__main__":
+    get_body_decode()
