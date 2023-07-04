c6fd28f452573e9b479f0c8cd9e66c506cbe0318
Johnstone Wahome <johnstonewahome@gmail.com>
2023-07-04 17:58:28 +0300

    post emails

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 # Python script that takes in a URL and an email, sends a POST
-import urllib.request
-import urllib.parse
-import sys
+from urllib.request import urlopen, Request
+from urllib.error import URLError
+from urllib.parse import urlencode
+from sys import argv
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
+def make_post_request(url_str: str, email_arg: str):
+    try:
+        data = urlencode({"email": email_arg})
+        email = data.encode("utf-8")
+        req = Request(url_str, email)
+        with urlopen(req) as response:
+            body = response.read()
+        print(body.decode("utf-8"))
+    except URLError as e:
+        if hasattr(e, "reason"):
+            print(e.reason)
+        elif hasattr(e, "code"):
+            print(e.code)
 
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
 
-print('Response body:')
-print(body)
+if __name__ == "__main__":
+    try:
+        make_post_request(argv[1], argv[2])
+    except IndexError:
+        raise
