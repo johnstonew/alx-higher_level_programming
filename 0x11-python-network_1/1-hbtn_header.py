70c32c5e0701b2b4632b8a249580d83c2c428a5e
Johnstone Wahome <johnstonewahome@gmail.com>
2023-07-04 17:57:02 +0300

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """script that makes a request to the url and gets a header value"""
-import urllib.request
-import sys
+from urllib.request import urlopen, Request
+from urllib.error import URLError
+from sys import argv
 
-url = sys.argv[1]
 
-req = urllib.request.Request(url)
+def get_header_value(url_str: str):
+    try:
+        req = Request(url_str)
+        with urlopen(req) as response:
+            body = response.read()
+        print(response.info().get("X-Request-Id"))
+    except URLError as e:
+        if hasattr(e, "reason"):
+            print(e.reason)
+        elif hasattr(e, "code"):
+            print(e.code)
 
-with urllib.request.urlopen(req) as response:
-    x_request_id = response.headers.get('X-Request-Id')
 
-print('X-Request-Id:', x_request_id)
+if __name__ == "__main__":
+    get_header_value(argv[1])
